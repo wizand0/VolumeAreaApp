e49ac9054052b3136a766bf3130acf4eb1984035
wizand0 <xabor@yandex.ru>
2024-08-19 08:57:40 +0300

    Logic for 4 shapes

--- app/src/main/java/ru/wizand/volumeareaapp/Prism.java
+++ app/src/main/java/ru/wizand/volumeareaapp/Prism.java
@@ -1,6 +1,10 @@
 package ru.wizand.volumeareaapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Prism extends AppCompatActivity {
 
+
+    EditText first, second, third;
+    TextView title, result;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_prism);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        first = findViewById(R.id.editTextFirst);
+        second = findViewById(R.id.editTextSecond);
+        third = findViewById(R.id.editTextThird);
+        title = findViewById(R.id.textView2);
+        result = findViewById(R.id.textView3);
+        btn = findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1 = first.getText().toString();
+                Double r1 = Double.parseDouble(s1);
+
+                String s2 = first.getText().toString();
+                Double r2 = Double.parseDouble(s2);
+
+                String s3 = first.getText().toString();
+                Double r3 = Double.parseDouble(s3);
+
+
+
+                double volume = r1*r2*r3;
+                result.setText("V = "+volume+" m^3");
+            }
         });
+
     }
 }
